79510249ec66d3331aa26e7977c548f5d111072a
bp2008_cp <SND\bp2008_cp>
2015-12-03 14:44:13 +0000

    Initial checkin.

---
+++ UI2Service/UI2ServiceWrapper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BPUtil;
+using UI2Service.Configuration;
+
+namespace UI2Service
+{
+	public class UI2ServiceWrapper
+	{
+		public static string Version = "0.1";
+		public UI2Server httpServer;
+		//UI2WebSocketServer webSocketServer;
+		public static UI2ServiceConfig cfg;
+
+		public UI2ServiceWrapper()
+		{
+			System.Net.ServicePointManager.Expect100Continue = false;
+			System.Net.ServicePointManager.DefaultConnectionLimit = 640;
+			Logger.logType = LoggingMode.Console | LoggingMode.File;
+			AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+			cfg = new UI2ServiceConfig();
+			if (File.Exists(Globals.ConfigFilePath))
+				cfg.Load(Globals.ConfigFilePath);
+			cfg.Save(Globals.ConfigFilePath);
+
+			BPUtil.SimpleHttp.SimpleHttpLogger.RegisterLogger(Logger.httpLogger);
+
+			//BPUtil.SimpleHttp.GlobalThrottledStream.ThrottlingManager.Initialize(3);
+			//BPUtil.SimpleHttp.GlobalThrottledStream.ThrottlingManager.SetBytesPerSecond(0, cfg.options.uploadBytesPerSecond);
+			//BPUtil.SimpleHttp.GlobalThrottledStream.ThrottlingManager.SetBytesPerSecond(1, cfg.options.downloadBytesPerSecond);
+			//BPUtil.SimpleHttp.GlobalThrottledStream.ThrottlingManager.SetBytesPerSecond(2, -1);
+			//BPUtil.SimpleHttp.GlobalThrottledStream.ThrottlingManager.BurstIntervalMs = cfg.options.throttlingGranularity;
+		}
+		#region Start / Stop
+		public void Start()
+		{
+			Stop();
+
+			httpServer = new UI2Server(cfg.webport, cfg.webport_https, cfg.blueIrisBaseURL);
+			httpServer.Start();
+
+			//webSocketServer = new UI2WebSocketServer(cfg.webSocketPort, cfg.webSocketPort_secure);
+			//webSocketServer.Start();
+
+			Logger.StartLoggingThreads();
+		}
+		public void Stop()
+		{
+			if (httpServer != null)
+			{
+				httpServer.Stop();
+				httpServer.Join(1000);
+			}
+
+			//// webSocketServer does not support closing/stopping
+			//webSocketServer = null;
+
+			Logger.StopLoggingThreads();
+		}
+		#endregion
+
+		static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+		{
+			if (e.ExceptionObject == null)
+			{
+				Logger.Debug("UNHANDLED EXCEPTION - null exception");
+			}
+			else
+			{
+				try
+				{
+					Logger.Debug((Exception)e.ExceptionObject, "UNHANDLED EXCEPTION");
+				}
+				catch (Exception ex)
+				{
+					Logger.Debug(ex, "UNHANDLED EXCEPTION - Unable to report exception of type " + e.ExceptionObject.GetType().ToString());
+				}
+			}
+		}
+	}
+}
---
+++ UI2Service/UI2WebSocketServer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BPUtil;
+using Fleck;
+
+namespace UI2Service
+{
+	public class UI2WebSocketServer
+	{
+		WebSocketServer ws_insecure;
+		WebSocketServer wss_secure;
+
+		public UI2WebSocketServer(int port, int secure_port = -1, System.Security.Cryptography.X509Certificates.X509Certificate2 cert = null)
+		{
+			FleckLog.logStuff = false;
+
+			if (port > 65535 || port < -1) port = -1;
+			if (secure_port > 65535 || secure_port < -1) secure_port = -1;
+
+			if (port > -1)
+			{
+				ws_insecure = new WebSocketServer("ws://localhost:" + port);
+			}
+
+			if (secure_port > -1)
+			{
+				wss_secure = new WebSocketServer("wss://localhost:" + secure_port);
+				wss_secure.Certificate = cert != null ? cert : BPUtil.SimpleHttp.HttpServer.GetSelfSignedCertificate();
+			}
+		}
+
+		/// <summary>
+		/// Starts listening for connections.
+		/// </summary>
+		public void Start()
+		{
+			if (ws_insecure != null)
+				ws_insecure.Start(handler);
+			if (wss_secure != null)
+				wss_secure.Start(handler);
+		}
+		/// <summary>
+		/// Handles incoming requests.
+		/// </summary>
+		/// <param name="socket"></param>
+		public void handler(IWebSocketConnection socket)
+		{
+			socket.OnOpen = () =>
+			{
+				Console.WriteLine("WebSocket Open!");
+			};
+			socket.OnClose = () =>
+			{
+				Console.WriteLine("WebSocket Close!");
+			};
+			socket.OnMessage = message =>
+			{
+				try
+				{
+					Console.WriteLine("WebSocket message: " + message);
+				}
+				catch (Exception ex)
+				{
+					Logger.Debug(ex);
+				}
+			};
+		}
+	}
+}
